7edaa861dbab5ab3d131840df0296fa424a65507
Midhunpm6 <midhunpm6060@gmail.com>
2025-07-14 02:07:12 +0530

    feat: Create multiple chatroom flow and fetch chatrooms,and used redis to get cache data

---
+++ src/config/redisClient.js
@@ -0,0 +1,12 @@
+import Redis from 'ioredis'
+
+export const  redisClient = new Redis({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined
+})
+
+redisClient.set('mykey', 'Hello, Redis!')
+redisClient.get('mykey').then(result => {
+  console.log('Redis says:', result)
+})
